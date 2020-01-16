b45b3ee13cf8393dbe0aefac5e7a6863fdd1f0c2
Caroline Sandford <caroline.sandford@metoffice.gov.uk>
2020-01-16 16:20:24 +0000

    Removed unused import.

--- improver_tests/psychrometric_calculations/test_WetBulbTemperature.py
+++ improver_tests/psychrometric_calculations/test_WetBulbTemperature.py
@@ -40,7 +40,6 @@ from iris.tests import IrisTest
 
 from improver.psychrometric_calculations.psychrometric_calculations import (
     WetBulbTemperature)
-from improver.utilities.warnings_handler import ManageWarnings
 
 from ..set_up_test_cubes import set_up_variable_cube
 
